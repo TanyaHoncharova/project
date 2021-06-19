8c8ef6880855ed0676c64f4dc586f0cb401e84de
TanyaHoncharova <tanushaGK@gmail.com>
2021-06-19 19:53:01 +0300

    add all

---
+++ src/scripts/get-refs.js
@@ -0,0 +1,12 @@
+export default function getRefs() {
+  return {
+    eventsList: document.querySelector('.js-events-list'),
+    paginationContainer: document.getElementById('pagination'),
+    lightbox: document.querySelector('.js-lightbox'),
+    lightboxContent: document.querySelector('.lightbox__content'),
+    closeLightboxBtn: document.querySelector('[data-action="close-lightbox"]'),
+    searchQuery: document.querySelector('.search-input'),
+    lightboxBackdrop: document.querySelector('div.lightbox__overlay'),
+    eventCard: document.querySelector('.event-card'),
+  };
+}
---
+++ src/scripts/logic-row-search-keyword.js
@@ -0,0 +1,36 @@
+import NewApiService from './apiService';
+import getRefs from './get-refs';
+import debounce from 'lodash.debounce';
+import eventsTpl from '../templates/events.hbs';
+import Swal from 'sweetalert2';
+
+const refs = getRefs();
+
+const apiService = new NewApiService();
+
+refs.searchQuery.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+    apiService.query = e.target.value;
+    apiService.fetchEventsDefault().then(renderEventsCard).catch(onError);
+}
+
+function onError() {
+    Swal.fire('Oops...', 'Nothing was found for your query! Try again...', 'error');
+      resetInput();
+      resetKeyword();
+      resetPage();
+}
+function renderEventsCard(events) {
+    const markup = eventsTpl(events);
+    refs.eventsList.innerHTML = markup;
+}
+function resetInput() {
+    refs.searchQuery.value = '';
+}
+function resetKeyword() {
+    apiService.query = "";
+}
+function resetPage() {
+    apiService.fetchEventsDefault().then(renderEventsCard);
+}
